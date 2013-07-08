152f339f70fab268fda3491a60fbbb5a8981bc29
Neetika Bansal <neetikabansal2491@gmail.com>
2013-07-08 06:01:35 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+fs = require("fs");
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var content = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
